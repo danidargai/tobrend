1a71bebde331894a9e2e361f43024c28d85e91a6
Dargai Dániel <dargaidanielferenc@ktch.hu>
2024-04-18 16:07:20 +0200

    Vége

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const doc = {
+    bookBody: document.querySelector('#bookBody')
+}
+
+
+
+
+
+
+const books = [
+    {id:1,title: "A fehér báró", author: "Jókai mór"},
+    {id:2,title: "Mégis mozog a föld", author: "Jókai mór"},
+    {id:3,title: "Fekete arany", author: "Jókai mór"}
+]
+
+books.forEach(book => {
+    let tr = document.createElement('tr')
+    tr.innerHTML = `
+    <td>${book.id}</td>
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    
+    
+    `
+    doc.bookBody.appendChild(tr)
+})
